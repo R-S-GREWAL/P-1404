f3ad9040be4f8d628daf9d1a8dc9ad1522fd8eb7
Rehan S. Grewal <88488978+R-S-GREWAL@users.noreply.github.com>
2023-06-08 16:13:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+function setup() {
+  canvas = createCanvas(1240,336);
+	canvas.parent('canvas');
+	video = createCapture(VIDEO);
+	video.size(900,325);
+	video.parent('game_console');
+	
+  poseNet = ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose',gotPoses);
+  
+}
+function modelLoaded()
+{
+  console.log('Model Loaded!');
+  poseNet.on('pose,gotPoses');
+}
